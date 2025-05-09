a3f1ec34532a7385a1a10458207968391f1be275
AwAjie <139576615+ajdgg@users.noreply.github.com>
2025-05-10 00:52:47 +0800

    fix: 去除input中多余的i18n文字

--- SpecialView.php
+++ SpecialView.php
@@ -122,7 +122,6 @@ class SpecialView extends \SpecialPage {
 			'name' => 'user',
 			'id' => 'user',
 			'value' => $user,
-			'placeholder' => $this->msg('viewavatar-username')->text(),
 		]),new OOUI\ButtonInputWidget([
 			'label' => $this->msg('viewavatar-submit')->text(),
 			'type' => 'submit',
